029e3969e2118a92e81eb12c21496a7dab536543
Azatov Murodbek <murodbekazatov126@gmail.com>
2025-09-13 15:30:05 +0500

    Add files via upload

---
+++ MASALALAR YECHISH.py
@@ -0,0 +1,45 @@
+# 1-masala sonni kiritib tekshirish
+#if (n := int(input("Son kiriting: "))) > 0:
+#    print(f"Musbat son: {n}")
+
+# 2-masala so'z uzunligini tekshirish
+#if (s := input("So‘z kiriting: ")) and len(s) > 5:
+#    print(f"So‘z uzun: {s}")
+
+# 3-masala juft yoki toq
+#if (son := int(input("Son kiriting:"))) % 2 == 0:
+#    print("Juft")
+#else:
+#    print("Toq")
+
+# 4-masala Ro‘yxat uzunligi
+#if len (lst := [1,2,3,4]) >3:
+#    print("Ro‘yxat uzunligi:",len(lst))
+
+# 5-masala Matn ichida so‘z bor-yo‘qligini aniqlash
+#if"salom" in (matn := input("Matn kiriting:")):
+#    print("Matnda 'salom' so'zi bor ")
+
+# 6-masala Son kvadrati
+#if (son := int(input("Son kiriting:"))) > 0:
+#    print("Kvadrati:", son**2)
+
+# 7-masala Yoshni tekshirish
+#if (yosh := int(input("Yosh kiriting:"))) >= 18:
+#    print("Kirishingiz mumkin")
+
+# 8-masala Ro‘yxatdan element olish
+#if (birinchi := [1, -3, 5][0]) > 0:
+#    print("Birinchi element", birinchi)
+
+# 9-masala Matn uzunligini chiqarish
+#if len(matn := input("Matn kiriting:")) > 0:
+#    print("Matn uzunligi", len(matn))
+
+# 10-masala Minimal sonni topish
+#a = int(input("1-son:"))
+#b = int(input("2-son:"))
+#c = int(input("3-son:"))
+
+#if(min_son := min(a, b, c)):
+#    print("Eng kichik son", min_son)
